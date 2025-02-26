adee1d476444240e768939d13a8d0be1b582e8fa
JPatit <joaopatomoscardini@gmail.com>
2025-02-26 02:34:08 +0000

    Ajeitei?

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 int main() {
-    printf("Desafio Super Trunfo\n");
+
+    printf("Carta 1:\n");
+    char Estado = 'A';
+    char Código[10] = "A01";
+    char Cidade[10] = "São Paulo";
+    int População =  12325000;
+    float Área = 1521.11;
+    float PIB = 699.28;
+    int Turismo = 50;
+
+    printf("Estado: %c\n", Estado);
+    printf("Código: %s\n", Código);
+    printf("Cidade: %s\n", Cidade);
+    printf("População %d\n", População);
+    printf("Área: %2.fkm\n", Área);
+    printf("PIB: %f\n", PIB);
+    printf("Turismo: %d\n", Turismo);
 
     return 0;
 }
