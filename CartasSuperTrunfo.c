ebfff7de9ab9f4b0828351a47e3ea93a550cf066
JPatit <joaopatomoscardini@gmail.com>
2025-03-12 02:25:23 +0000

    Desafio Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,20 +3,39 @@ int main() {
 
     printf("Carta 1:\n");
     char Estado = 'A';
-    char Código[10] = "A01";
-    char Cidade[10] = "São Paulo";
-    int População =  12325000;
-    float Área = 1521.11;
-    float PIB = 699.28;
-    int Turismo = 50;
+    char Codigo[10] = "A01";
+    char Cidade[10] = "Sao Paulo";
+    int Populacao = 22990000;
+    float Area = 1521.11;
+    float PIB = 1.6;
+    int Turismo = 36;
 
     printf("Estado: %c\n", Estado);
-    printf("Código: %s\n", Código);
+    printf("Codigo: %s\n", Codigo);
     printf("Cidade: %s\n", Cidade);
-    printf("População %d\n", População);
-    printf("Área: %2.fkm\n", Área);
-    printf("PIB: %f\n", PIB);
+    printf("Populacao %d\n", Populacao);
+    printf("Area: %2.f km\n", Area);
+    printf("PIB: %.2f trilhoes\n", PIB);
     printf("Turismo: %d\n", Turismo);
+
+    printf("\n");
+
+    printf("Carta 2:\n");
+    char estado = 'B';
+    char codigo [10] = "B01";
+    char cidade [20] = "Rio de Janeiro";
+    int populacao = 6211200;
+    float area = 1200;
+    float pib = 1.15;
+    int turismo = 58;
+
+    printf("Estado: %c\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao %d\n", populacao);
+    printf("Area: %2.f km\n", area);
+    printf("PIB: %.2f trilhoes\n", pib);
+    printf("Turismo: %d\n", turismo);
 
     return 0;
 }
