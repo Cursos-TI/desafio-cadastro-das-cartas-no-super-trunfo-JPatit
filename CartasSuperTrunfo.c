8b9e4258f4a2dbdccc677e29a991baddb92f052d
JPatit <joaopatomoscardini@gmail.com>
2025-03-28 01:46:57 +0000

    desafio nivel 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,18 +5,22 @@ int main() {
     char Estado = 'A';
     char Codigo[10] = "A01";
     char Cidade[10] = "Sao Paulo";
-    int Populacao = 22990000;
-    float Area = 1521.11;
-    float PIB = 1.6;
+    int Populacao = 11895578;
+    float Area = 1521.20;
+    float PIB = 857.62;
     int Turismo = 36;
+    float Densidade_Populacional = (float) Populacao / Area;
+    float PIB_per_capita = (PIB * 1000000000) / Populacao;
 
     printf("Estado: %c\n", Estado);
     printf("Codigo: %s\n", Codigo);
     printf("Cidade: %s\n", Cidade);
     printf("Populacao %d\n", Populacao);
-    printf("Area: %2.f km\n", Area);
-    printf("PIB: %.2f trilhoes\n", PIB);
+    printf("Area: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhoes\n", PIB);
     printf("Turismo: %d\n", Turismo);
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional);
+    printf("PIB per capita: %.2f reais\n", PIB_per_capita);
 
     printf("\n");
 
@@ -24,18 +28,22 @@ int main() {
     char estado = 'B';
     char codigo [10] = "B01";
     char cidade [20] = "Rio de Janeiro";
-    int populacao = 6211200;
+    int populacao = 6729894;
     float area = 1200;
-    float pib = 1.15;
+    float pib = 400.00;
     int turismo = 58;
+    float densidade_populacional = (float) populacao / area;
+    float Pib_per_capita = (pib * 1000000000) / populacao;
 
     printf("Estado: %c\n", estado);
     printf("Codigo: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
     printf("Populacao %d\n", populacao);
-    printf("Area: %2.f km\n", area);
-    printf("PIB: %.2f trilhoes\n", pib);
+    printf("Area: %.f km²\n", area);
+    printf("PIB: %.2f bilhoes\n", pib);
     printf("Turismo: %d\n", turismo);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
+    printf("PIB per capita: %.2f reais\n", Pib_per_capita);
 
     return 0;
 }
